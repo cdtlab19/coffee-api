313244914ea217b0e9af5b294930d8a2c3483251
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2019-04-04 14:05:31 -0300

    UserDTO created

--- src/main/java/com/cdtlab/coffeeapi/dto/UserDTO.java
+++ src/main/java/com/cdtlab/coffeeapi/dto/UserDTO.java
@@ -1,8 +1,13 @@
 package com.cdtlab.coffeeapi.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * UserDTO
  */
+@Data
+@NoArgsConstructor
 public class UserDTO {
 
     private String name;
