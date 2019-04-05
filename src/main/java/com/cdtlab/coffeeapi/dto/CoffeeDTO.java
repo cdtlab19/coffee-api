564f423d8ba8590a2457a0b98485fffcb0da0068
Alexandre Santos <mrxandejp@gmail.com>
2019-04-04 23:12:19 -0300

    Test branch alexandre

--- src/main/java/com/cdtlab/coffeeapi/dto/CoffeeDTO.java
+++ src/main/java/com/cdtlab/coffeeapi/dto/CoffeeDTO.java
@@ -1,8 +1,13 @@
 package com.cdtlab.coffeeapi.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * CoffeeDTO
  */
+@Data
+@NoArgsConstructor
 public class CoffeeDTO {
     private String id;
     private String type;
