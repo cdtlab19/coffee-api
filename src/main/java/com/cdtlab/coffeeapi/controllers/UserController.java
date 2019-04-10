73a8bc4a4a733e5b4cb5228477b91e6264a04751
João Vitor Oliveira <jvanjos@live.com>
2019-04-10 17:08:53 -0300

    doing user

--- src/main/java/com/cdtlab/coffeeapi/controllers/UserController.java
+++ src/main/java/com/cdtlab/coffeeapi/controllers/UserController.java
@@ -21,52 +21,96 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/api/users")
 public class UserController {
 
-    // @Autowired
-    // UserService service;
+		@Autowired
+    private UserService service;
 
 	@RequestMapping(value="/", method=RequestMethod.POST)
     public ResponseEntity<?> createUser(@RequestBody UserDTO user, HttpServletRequest request) {
-	     AccountChainCode account = AccountConverts.toEntity(user);
-	
-	     account.setMethod("Create");
-	     return new ResponseEntity<>(chaincodeServices.invoke(sessionConnection.getConnectionFabric().getConnection(), sessionConnection.getChannelFabric().getChannel(), account), HttpStatus.OK);
+		try {
+        	SessionConnection sessionConnection = (SessionConnection) request.getSession()
+    				.getAttribute("SESSION_CONNECTION");
 
+    		if (sessionConnection == null) {
+    			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    		}
+	     return new ResponseEntity<>(service.CreateUser(user), HttpStatus.OK);
+		} catch (InvalidArgumentException | ProposalException | InterruptedException | ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return new ResponseEntity<>((List<Response>) e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         
     }
 
     @RequestMapping(value="/{id}", method=RequestMethod.PUT)
     public ResponseEntity<?> updateUser(@PathVariable String id, @RequestBody UserDTO user, HttpServletRequest request) {) {
-    		     
+    	public ResponseEntity<?> deleteUser(@PathVariable String id, HttpServletRequest request) {
+        	try {
+            	SessionConnection sessionConnection = (SessionConnection) request.getSession()
+        				.getAttribute("SESSION_CONNECTION");
+
+        		if (sessionConnection == null) {
+        			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        		}
+    	     return new ResponseEntity<>(service.UpdateUser(user), HttpStatus.OK);
+    		} catch (InvalidArgumentException | ProposalException | InterruptedException | ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return new ResponseEntity<>((List<Response>) e, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
     }
 
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
     public ResponseEntity<?> deleteUser(@PathVariable String id, HttpServletRequest request) {
-    	AccountChainCode account = AccountConverts.toEntity(user);
-    	
-	    account.setMethod("Delete");
+    	try {
+        	SessionConnection sessionConnection = (SessionConnection) request.getSession()
+    				.getAttribute("SESSION_CONNECTION");
 
-	    return new ResponseEntity<>(chaincodeServices.invoke(sessionConnection.getConnectionFabric().getConnection(), sessionConnection.getChannelFabric().getChannel(), account), HttpStatus.OK);
-
+    		if (sessionConnection == null) {
+    			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    		}
+	     return new ResponseEntity<>(service.DeleteUser(id), HttpStatus.OK);
+		} catch (InvalidArgumentException | ProposalException | InterruptedException | ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return new ResponseEntity<>((List<Response>) e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    @GetMapping(value="/{id}")
     public ResponseEntity<?> getUser(@PathVariable String id, HttpServletRequest request) {
-    	AccountChainCode account = AccountConverts.toEntity(user);
-    	
-	    account.setMethod("GetByUser");
+    	public ResponseEntity<?> deleteUser(@PathVariable String id, HttpServletRequest request) {
+        	try {
+            	SessionConnection sessionConnection = (SessionConnection) request.getSession()
+        				.getAttribute("SESSION_CONNECTION");
 
-	    return new ResponseEntity<>(chaincodeServices.query(sessionConnection.getConnectionFabric().getConnection(), sessionConnection.getChannelFabric().getChannel(), account), HttpStatus.OK);
-        
+        		if (sessionConnection == null) {
+        			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        		}
+    	     return new ResponseEntity<>(service.GetUser(id), HttpStatus.OK);
+    		} catch (InvalidArgumentException | ProposalException | InterruptedException | ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return new ResponseEntity<>((List<Response>) e, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
     }
 
-    @RequestMapping(value="/", method=RequestMethod.GET)
+    @GetMapping(value="/")
     public ResponseEntity<?> allUsers(HttpServletRequest request) {
-    	AccountChainCode account = AccountConverts.toEntity(user);
-    	
-	    account.setMethod("GetAllUsers");
+    	public ResponseEntity<?> deleteUser(@PathVariable String id, HttpServletRequest request) {
+        	try {
+            	SessionConnection sessionConnection = (SessionConnection) request.getSession()
+        				.getAttribute("SESSION_CONNECTION");
 
-	    return new ResponseEntity<>(chaincodeServices.query(sessionConnection.getConnectionFabric().getConnection(), sessionConnection.getChannelFabric().getChannel(), account), HttpStatus.OK);
-        
+        		if (sessionConnection == null) {
+        			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        		}
+    	     return new ResponseEntity<>(service.AllUser(id), HttpStatus.OK);
+    		} catch (InvalidArgumentException | ProposalException | InterruptedException | ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return new ResponseEntity<>((List<Response>) e, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
 
     }
 
--- src/main/java/com/cdtlab/coffeeapi/path/ChaincodeList.java
+++ src/main/java/com/cdtlab/coffeeapi/path/ChaincodeList.java
@@ -1,5 +1,6 @@
 package com.cdtlab.coffeeapi.path;
 
 public class ChaincodeList {
-
+		public static final String CC_USER = "user";
+		public static final String CC_COFFE = "coffe";		
 }
